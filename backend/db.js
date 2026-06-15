2112bfbdad1bb925bbda033d3dbdd50fededee59
Desquita Sasyili <sasyilikheyla@gmail.com>
2026-06-15 09:41:52 +0700

    perubahan db

--- backend/db.js
+++ backend/db.js
@@ -3,8 +3,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+console.log("HOST:", process.env.DB_HOST);
+console.log("PORT:", process.env.DB_PORT);
+console.log("USER:", process.env.DB_USER);
+console.log("DB:", process.env.DB_NAME);
+
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT, // tambahin ini
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
